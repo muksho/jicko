79ca0500a16f6fe891f0456b7ec2f35ed82caf10
muksho <30629778+muksho@users.noreply.github.com>
2017-12-09 22:37:18 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="en">
+<!-- Starting of head -->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>About Us</title>
+	    <meta name="Description" content="IT service provider in dhanbad" />
+	    <meta name="Keywords" content="Dhanbad" />
+		<meta name="viewport" content="width=device-width">
+		
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/responsive.css">
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+		<!--script src="css/slide.js"></script-->
+		
+    
+	</head>
+<!-- Head Finished-->
+
+	<body>
+		
+		<header class="Main">
+			<div class="width-container">
+				<div id="logo"><a href="index-2.html"><img src="images/logo.png" alt="Sarojanil Technologies"></a></div>
+				
+				<nav>
+					<ul class="sf-menu">
+						<li class="current-menu-item">
+							<a href="index.html">Home</a>
+						</li>
+	                    
+	                    <li>
+							<a href="about.html">About Us</a>
+						</li>
+	                    <li>
+							<a href="#.html">IT Services</a>
+							<ul>
+								<li>
+									<a href="#.html">Website Development</a>
+								</li>
+								<li>
+									<a href="#.html">Software Development</a>
+								</li>
+								<li>
+									<a href="#.html">Search Engine Optimisation</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#.html">Security Services</a>
+							<ul>
+								<li>
+									<a href="#.html">CCTV Cameras</a>
+								</li>
+								<li>
+									<a href="#.html">Time Attendance Rocorder</a>
+								</li>
+								<li>
+									<a href="#.html">Face Recognisation Device</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="FAQ.html">Portfolio</a>
+						</li>
+	                    <li>
+							<a href="FAQ.html">FAQ</a>
+						</li>
+						<li>
+							<a href="contact-us.html">Contact Us</a>
+						</li>
+					</ul>
+					<select class="select-menu">
+						<option value="#">
+							<span>Menu</span>
+						</option>
+						<option value="404.html">
+							&nbsp;Home
+						</option>
+						<option value="404.html">
+							&nbsp;About us
+						</option>
+						<option value="404.html">
+							&nbsp;IT Sevvices
+						</option>
+						<option value="404.html">
+							&nbsp;Security Services
+						</option>
+						<option value="404.html">
+							&nbsp;Portfolio
+						</option>
+						<option value="404.html">
+							&nbsp;FAQ
+						</option>
+						<option value="404.html">
+							&nbsp;Contact Us
+						</option>
+					</select>
+				</nav>
+				
+				
+			
+			<div class="clearfix"></div>
+			</div><!-- close .width-container -->
+		</header>
+
+		<div id="contact-header">
+			<div class="width-container"><div id="contact-header-text"><strong>Address:</strong> <em><a href="https://goo.gl/itJtT6" target="_blank">2nd Floor, Raj Complex,Beside NEXA Showroom,Main Road Kolakusma,Saraidhela, Dhanbaad,Jharkhand - 828127</a></em>  &nbsp;&nbsp;&nbsp;&nbsp; <strong>Contact:</strong> <em>+91-7004425367</em></div></div><!-- close .width-container -->
+		</div>
+		<div class="container" style="text-align: justify;">
+			<div style="background-color: white;">
+				
+					<h2 class="title-anchor">WELCOME TO SAROJANIL TECHNOLOGY PVT. LTD.</h2>
+					<p>Sarojanil strongly committed to using its unique skills and talents to serve our customers and continued growth to our employees. 
+						We believe in offering technology to the masses.</p>
+					<p>As a web development company, we are experience in Web Designing, Graphic Designing, Application Development, Internet Marketing and E-Commerce Development can bring your ideas and concepts to reality. Along with creative designers and programmers we have content writer, web consultants and internet marketing professionals tobring you results for the online business.</p>
+				
+				
+			</div>
+			<div style="padding: 11px;"></div>
+			<div style="font-size: 12px;">
+				<div>
+					<h4>SOFTWARE DEVELOPMENT</h4>
+					<p>We have strong technical team who can analyze your business process and help in automating your various business processes viz. Human Resource Management, Supply Chain Management, Customer Relationship Management, Finance & Accounts Management, Manufacturing & Productions Management, Inventory Management, Project Management and all other business processes.</p>
+				</div>
+				<div style="padding: 3px;"></div>
+				<div>
+					<h4>WEBSITE</h4>
+					<p>We provide end to end of Website Solutions viz. Domain, Hosting, Website Design & Development. We understand that website is more than just a web presence for you, hence we try to portray and project your business to your target through the website. We intend to make website as your sales engine. We have experienced designers who can come up with creative, professional and user friendly websites.</p>
+				</div>
+				<div style="padding: 3px;"></div>
+				<div>
+					<h4>ERP SOLUTIONS</h4>
+					<p>Sarojanil provides custom ERP solutions across various verticals viz. Education, Real Estate, Manufacturing, Clinical Research, Healthcare and Retail. We have our own cloud based ERP products related to Real Estate and Education sector. We provide end to end ERP solution ranging from Consulting, Analysis, Design, Development, Quality Analysis and Deployment.</p>
+				</div>
+				<div style="padding: 3px;"></div>
+				<div>
+					<h4>E-COMMERCE</h4>
+					<p>With our Ecommerce solution, you can start selling online right away. Our Ecommerce solution is created with essential business functions in a strong back end system, tested successfully, and made affordable. Our Ecommerce solutions are very contemporary and loaded with features viz. product management, inventory management, customer management, order management, analytics and reports.</p>
+				</div>
+				<div style="padding: 3px;"></div>
+				<div>
+					<h4>INTERNET MARKETING</h4>
+					<p>Our Internet Marketing solutions will help you to increase your presence online manifold times, and hence increase in lead generation for your business. Our Internet Marketing solution includes Search Engine Optimization, Social Media Optimization, Facebook Promotions, Email Marketing and SMS Marketing.</p>
+				</div>
+				<div style="padding: 3px;"></div>
+				<div>
+					<h4>CLOUD SOLUTIONS</h4>
+					<p>We provide end to end of cloud solutions ranging from cloud based product application development, deploying and configuring the cloud servers, and maintaining the cloud applications. We have excellent expertise in various cloud platforms viz. Amazon, Windows Azure, Rackspace etc. We also have our own cloud based products solutions portfolio.</p>
+				</div>
+			</div>
+		</div>
+	<!-- footer-->
+	<hr style="height:2px; background-color: red; border:none;color:#ec7201">
+	<br class="spacer">
+	     <div class="footer" style="text-align: center;">
+	        <p>
+	        	 Designed and Developed by <a href="https://www.facebook.com/shouvik.mukherjee.77" >Shouvik Mukherjee</a>
+	        	 <br>
+	        	&copy; 2017 Demo Hotel<sup>&reg;</sup>. All Rights Reserved.<br />
+	            </p>
+	                     
+	            <br class="spacer">
+	    </div>
+	</body>
+</html>
